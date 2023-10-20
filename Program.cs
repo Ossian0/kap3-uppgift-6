e5e0a6da589159918757417b0672e7cb4fd2e67e
Admin <Admin@TEB123017>
2023-10-20 14:52:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt förnamn och efternamn.");
+            string helanamn = Console.ReadLine();
+            int mellanslagIndex = helanamn.IndexOf(" ");
+            string förnamn = helanamn[..mellanslagIndex];
+            string efternamn = helanamn[(mellanslagIndex + 1)..];
+
+            if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == -1)
+            {
+                Console.WriteLine("Ditt förnamn kommer först i bokstavsordning.");
+            }
+            else if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == 0)
+            {
+                Console.WriteLine("Ditt förnamn och efternamn ligger på samma plats i bokstavs ordning.");
+            }
+            else if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == 1)
+            {
+                Console.WriteLine("Ditt efternamn kommer först i bokstavsorndning.");
+            }
+            Console.WriteLine(förnamn + " " + efternamn);
+            Console.ReadKey();
+        }
+    }
+}
